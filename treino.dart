ef2e7e52a6cdd4687c08b8620a78072e79533e36
Amarauru <pedrohaa11@gmail.com>
2023-11-16 00:38:29 -0300

    att

---
+++ treino.dart
@@ -0,0 +1,16 @@
+void main(){
+String nome = "Maçã";
+int dias = 10;
+bool madura;
+
+
+void maduraFruta(String nome,int dias) {
+  
+  if (dias >= 30){
+    print("A $nome está madura.");
+  } else{
+    print ("A $nome não está madura");
+  }
+
+}
+}
